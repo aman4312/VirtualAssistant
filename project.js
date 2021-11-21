866b1b24915f0d1871a8ec5fa17bd586e9982689
Aman srivastava <srivastavaaman085@gmail.com>
2021-11-21 18:49:31 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,8 @@
+
+    responsiveVoice.speak("Hello I am Your Personal Assistant How May I help you?" );
+    var nm=prompt("What is your name?");
+    while(nm == null || nm == "") 
+{
+nm = prompt("What is your name?")
+}
+alert("Make sure volume is turned on.And ignore console error due to API.")
